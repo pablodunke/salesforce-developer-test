19c28e31977fbca0e22e55f19213a1ebbecc4a57
Pablo Dunke <32306594+pablodunke@users.noreply.github.com>
2020-03-29 15:52:25 -0300

    Update repository_gridController.js

--- aura/repository_grid/repository_gridController.js
+++ aura/repository_grid/repository_gridController.js
@@ -1,17 +1,32 @@
 ({
-    doInit: function(component, event, helper) 
+	doInit: function(component, event, helper) 
     {
-        var action = component.get("c.loadComponent");
+        component.set('v.columns', [
+            {label: 'Name', fieldName: 'name', type: 'text'},
+            {label: 'Forks', fieldName: 'forks', type: 'number'},
+            {label: 'Stars', fieldName: 'stars', type: 'number'}
+        ]);
+        
+		var action = component.get("c.loadComponent");
 
-        action.setCallback(this, function(a)
+        action.setCallback(this, function(cb)
         {
-            var state = a.getState();
+            var state = cb.getState();
             if(state == "SUCCESS") 
             {
-                component.set("v.repositories", a.getReturnValue());
+                var response = cb.getReturnValue();
+     
+                var dataarray = [];
+                for(let i in response)
+                {
+                    console.log(response[i]);
+                    dataarray.push({"name": response[i].Name, "forks": response[i].Forks__c, "stars": response[i].Stars__c});
+                }
+
+                component.set('v.data', dataarray);
             }
         });
         
         $A.enqueueAction(action);
-    }
+	}
 })
