fad759b1c45cbd622de781a2824fa6b792968f15
Pablo Dunke <32306594+pablodunke@users.noreply.github.com>
2020-03-29 13:55:45 -0300

    Update repository_gridController.js

--- aura/repository_grid/repository_gridController.js
+++ aura/repository_grid/repository_gridController.js
@@ -1,6 +1,17 @@
 ({
-	myAction : function(component, event, helper) 
-	{
-		
+    doInit: function(component, event, helper) 
+    {
+        var action = component.get("c.loadComponent");
+
+        action.setCallback(this, function(a)
+        {
+            var state = a.getState();
+            if(state == "SUCCESS") 
+            {
+                component.set("v.repositories", a.getReturnValue());
+            }
+        });
+        
+        $A.enqueueAction(action);
 	}
 })
